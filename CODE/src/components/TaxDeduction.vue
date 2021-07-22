dac80fddef5fe6ca5a0baca88388d28350b2697c
Daniil <daniil2243@mail.ru>
2021-07-22 18:14:28 +0300

    UPDATE 0.3

--- CODE/src/components/TaxDeduction.vue
+++ CODE/src/components/TaxDeduction.vue
@@ -221,7 +221,6 @@
     }
 
     .custom-checkbox:checked+label::before {
-        border-color: #0b76ef;
         background: linear-gradient(255.35deg, #DC3131 0.83%, rgba(255, 79, 79, 0) 108.93%), #FF5E56;
         background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
     }
